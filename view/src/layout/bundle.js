ecdabfa386973629a59dc65b11e0599c382c412d
ckcks12 <wyoducc@gmail.com>
2017-03-22 23:06:27 +0900

    *add rename vue components according to.... as below

--- view/src/layout/bundle.js
+++ view/src/layout/bundle.js
@@ -19,15 +19,15 @@ window.jQuery = window.$;
 Vue.component('mainmenu', require('../vues/mainmenu.vue'));
 Vue.component('submenu', require('../vues/submenu.vue'));
 Vue.component('filtermenu', require('../vues/filtermenu.vue'));
-Vue.component('flame-graph', require('../vues/flameGraph.vue'));
+Vue.component('flame-graph', require('../vues/flame-graph.vue'));
 Vue.component('activity-map', require('../vues/activity-map.vue')); 
 Vue.component('histogram', require('../vues/histogram.vue'));
-Vue.component('screen-mode-viewer', require('../vues/screenModeViewer.vue'));
-Vue.component('top-header', require('../vues/topHeader.vue'));
-Vue.component('left-header', require('../vues/leftHeader.vue'));
-Vue.component('page-toolbar', require('../vues/pageToolbar.vue'));
-Vue.component('right-header', require('../vues/rightHeader.vue'));
-Vue.component('bottom-header', require('../vues/bottomHeader.vue'));
+Vue.component('screen-mode-viewer', require('../vues/screen-mode-viewer.vue'));
+Vue.component('top-header', require('../vues/top-header.vue'));
+Vue.component('left-header', require('../vues/left-header.vue'));
+Vue.component('page-toolbar', require('../vues/page-toolbar.vue'));
+Vue.component('right-header', require('../vues/right-header.vue'));
+Vue.component('bottom-header', require('../vues/bottom-header.vue'));
 
 /**
  * apply Vue app
