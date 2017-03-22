fd6e3574926e7c8ea88dda9a55ea50e3e65a145e
ckcks12 <wyoducc@gmail.com>
2017-03-22 17:10:11 +0900

    *add theme divided to components

--- view/src/layout/bundle.js
+++ view/src/layout/bundle.js
@@ -7,7 +7,8 @@ require('./style.scss');
  * import scripts
  */
 // Vue.js
-window.Vue = require('vue/dist/vue.min.js');
+// window.Vue = require('vue/dist/vue.min.js');
+window.Vue = require('vue/dist/vue.common.js');
 // jQuery
 window.$ = require('jquery');
 window.jQuery = window.$;
@@ -22,6 +23,11 @@ Vue.component('flameGraph', require('../vues/flameGraph.vue'));
 Vue.component('bucketGraph', require('../vues/bucketGraph.vue')); 
 Vue.component('histogram', require('../vues/histogram.vue'));
 Vue.component('screenModeViewer', require('../vues/screenModeViewer.vue'));
+Vue.component('topHeader', require('../vues/topHeader.vue'));
+Vue.component('leftHeader', require('../vues/leftHeader.vue'));
+Vue.component('pageToolbar', require('../vues/pageToolbar.vue'));
+Vue.component('rightHeader', require('../vues/rightHeader.vue'));
+Vue.component('bottomHeader', require('../vues/bottomHeader.vue'));
 
 /**
  * apply Vue app
---
+++ view/src/vues/bottomHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- BEGIN FOOTER -->
+    <div class="page-footer">
+        <div class="page-footer-inner"> 2017 &copy; Lantern By
+            <a target="_blank" href="http://thresh.io">Team Thresh</a> &nbsp;|&nbsp;Yu Yeong-su, Yu Geon-yeol, Lee Eun-chan
+        </div>
+        <div class="scroll-to-top">
+            <i class="icon-arrow-up"></i>
+        </div>
+    </div>
+    <!-- END FOOTER -->
+</template>
+
+<script>
+</script>
+
+<style lang="sass" scoped>
+</style>
