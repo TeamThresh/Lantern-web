9093efcd31dbef09922b032399e8f7383ac500fc
ckcks12 <wyoducc@gmail.com>
2017-03-29 00:32:14 +0900

    *add github link

--- view/src/vues/bottom-header.vue
+++ view/src/vues/bottom-header.vue
@@ -2,7 +2,11 @@
     <!-- BEGIN FOOTER -->
     <div class="page-footer">
         <div class="page-footer-inner"> 2017 &copy; Lantern By
-            <a target="_blank" href="http://thresh.io">Team Thresh</a> &nbsp;|&nbsp;Yu Yeong-su, Yu Geon-yeol, Lee Eun-chan
+            <a target="_blank" href="http://thresh.io">Team Thresh</a>
+			&nbsp;|&nbsp;
+			<a target="_blank" href="https://github.com/GDmail91">Yu Yeong-su</a>,
+			<a target="_blank" href="https://github.com/jratul">Yu Geon-yeol</a>,
+			<a target="_blank" href="https://github.com/ckcks12">Lee Eun-chan</a>
         </div>
         <div class="scroll-to-top">
             <i class="icon-arrow-up"></i>
