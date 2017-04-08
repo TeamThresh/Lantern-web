c5350635dec1d630683d9b38ae04e95a760f991c
ckcks12 <wyoducc@gmail.com>
2017-04-08 14:52:36 +0900

    *ing

--- routes/lantern.js
+++ routes/lantern.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('나도 준비중');
-  res.end();
+	res.send('hi');
+	// res.redirect('/dashboard');
+});
+
+router.get('/dashboard', function(req, res, next) {
+	res.send('hi');
+	res.end();
+	// res.render('view/dashboard/index.pug');
 });
 
 module.exports = router;
--- routes/thresh.js
+++ routes/thresh.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('준비중');
+  res.send('준비중2');
   res.end();
 });
 
