26681a1215bf160b5b3b13362345fdbd61b90c1f
ckcks12 <wyoducc@gmail.com>
2017-04-26 15:19:37 +0900

    *add filter menus and app information

--- view/vues/left-header.vue
+++ view/vues/left-header.vue
@@ -12,73 +12,33 @@
 			<!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
 			<!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
 			<!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
-			<a class="btn blue" id="btnAppName">
+			<!-- <a class="btn blue" id="btnAppName">
 				<i class="fa fa-dropbox fa-4"></i>
 				Dropbox
-			</a>
+			</a> -->
 			<ul class="page-sidebar-menu   page-header-fixed" data-keep-expanded="false" data-auto-scroll="false" data-slide-speed="200">
-				<li class="nav-item start">
-					<a href="/" class="nav-link">
-						<i class="icon-home"></i>
-						<span class="title">Dashboard</span>
-					</a>
-				</li>
-				<!-- Performance -->
 				<li class="heading">
-					<h3 class="uppercase">Performance</h3>
+					<h3 class="uppercase">Your App Information</h3>
 				</li>
-				<li class="nav-item  ">
-					<a href="/activityDetail/cpu" class="nav-link">
-						<i class="icon-layers"></i>
-						<span class="title">CPU/Memory</span>
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						<i class="fa fa-th-large"></i>
+						<span class="title">Kakaotalk</span>
 					</a>
 				</li>
 				<li class="nav-item">
-					<a href="/activityDetail/network" class="nav-link">
-						<i class="icon-feed"></i>
-						<span class="title">Network</span>
+					<a href="#" class="nav-link">
+						<i class="fa fa-code-fork"></i>
+						<span class="title">1.0.16</span>
 					</a>
 				</li>
-				<!-- Crash -->
 				<li class="heading">
-					<h3 class="uppercase">Crash</h3>
+					<h3 class="uppercase">Filter</h3>
 				</li>
 				<li class="nav-item">
-					<a href="/" class="nav-link">
-						<i class="icon-wrench"></i>
-						<span class="title">Crash</span>
-					</a>
-				</li>
-				<!-- User -->
-				<li class="heading">
-					<h3 class="uppercase">User</h3>
-				</li>
-				<li class="nav-item">
-					<a href="/" class="nav-link">
-						<i class="icon-user"></i>
-						<span class="title">User Flow</span>
-					</a>
-				</li>
-				<li class="nav-item">
-					<a href="/" class="nav-link">
-						<i class="icon-puzzle"></i>
-						<span class="title">Device Usage</span>
-					</a>
-				</li>
-				<!-- Report -->
-				<li class="heading">
-					<h3 class="uppercase">Report</h3>
-				</li>
-				<li class="nav-item">
-					<a href="/" class="nav-link">
-						<i class="icon-docs"></i>
-						<span class="title">Onepage Report</span>
-					</a>
-				</li>
-				<li class="nav-item">
-					<a href="/" class="nav-link">
-						<i class="icon-bar-chart"></i>
-						<span class="title">Statistics</span>
+					<a href="#" class="nav-link">
+						<i class="fa fa-filter"></i>
+						<span class="title">some menus</span>
 					</a>
 				</li>
 			</ul>
@@ -100,19 +60,19 @@
 	div.page-sidebar-wrapper {
 		display: inline !important;
 	}
-	a#btnAppName {
-		margin: 0;
-		padding: 0 0 0 20px;
-		width: 100%;
-		height: 50px;
-		display: block;
-		// background-color: #007eed;
-		text-align: left;
-		line-height: 50px;
-
-		& > i {
-			font-size: 27px;
-			vertical-align: middle;
-		}
-	}
+	// a#btnAppName {
+	// 	margin: 0;
+	// 	padding: 0 0 0 20px;
+	// 	width: 100%;
+	// 	height: 50px;
+	// 	display: block;
+	// 	// background-color: #007eed;
+	// 	text-align: left;
+	// 	line-height: 50px;
+	//
+	// 	& > i {
+	// 		font-size: 27px;
+	// 		vertical-align: middle;
+	// 	}
+	// }
 </style>
