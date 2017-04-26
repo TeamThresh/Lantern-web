29230f9e8a4044ee76a0a8dee2803582099a9ab3
ckcks12 <wyoducc@gmail.com>
2017-04-26 14:14:29 +0900

    *add new design

--- view/vues/line-graph.vue
+++ view/vues/line-graph.vue
@@ -11,21 +11,52 @@ module.exports = {
 	},
 	methods: {
 		draw: function() {
-			new Morris.Line({
-				element: this.$el,
-				data: [
-				    { y: '2006', a: 100, b: 90 },
-				    { y: '2007', a: 75,  b: 65 },
-				    { y: '2008', a: 50,  b: 40 },
-				    { y: '2009', a: 75,  b: 65 },
-				    { y: '2010', a: 50,  b: 40 },
-				    { y: '2011', a: 75,  b: 65 },
-				    { y: '2012', a: 100, b: 90 }
-				],
-				xkey: 'y',
-				ykeys: ['a', 'b'],
-				labels: ['Series A', 'Series B']
-			});
+			this.data = this.createSampleData();
+
+			let svg = d3.select(this.$el).append('svg');
+			let width = $(svg.node()).width();
+			let height = $(svg.node()).height();
+			let xScale = d3.scaleTime().domain([this.data[0].date, this.data[this.data.length - 1].date]).range([23, width - 10]);
+			let yScale = d3.scaleLinear().domain([0, 100]).range([height - 15, 10]);
+			let xAxis = d3.axisBottom(xScale).ticks(4).tickSize(0).tickFormat(d3.timeFormat('%H:%M'));
+			let yAxis = d3.axisLeft(yScale).ticks(1).tickSize(0);
+			svg.append('g').attr('class', 'x-axis').attr('transform', `translate(0, ${height - 13})`).call(xAxis)
+				.selectAll('line').remove();
+			svg.append('g').attr('class', 'y-axis').attr('transform', `translate(3, 0)`).call(yAxis)
+				.selectAll('line').remove();
+			let guideGroup = svg.append('g').attr('class', 'guide-group');
+			guideGroup.selectAll('line')
+				.data(svg.select('g.x-axis').selectAll('text').nodes()).enter().append('line')
+				.attr('x1', (d) => $(d).position().left - $(d).parent().parent().parent().position().left + 10)
+				.attr('x2', (d) => $(d).position().left - $(d).parent().parent().parent().position().left + 10)
+				.attr('y1', (d) => $(d).position().top - $(d).parent().parent().parent().position().top - 7)
+				.attr('y2', 10)
+				.attr('stroke-dasharray', '5, 5')
+				.attr('class', 'guide');
+			guideGroup.append('line').attr('class', 'guide')
+				.attr('x1', xScale(this.data[0].date)).attr('x2', xScale(this.data[this.data.length - 1].date))
+				.attr('y1', yScale(0)).attr('y2', yScale(0));
+			guideGroup.append('line').attr('class', 'guide')
+				.attr('x1', xScale(this.data[0].date)).attr('x2', xScale(this.data[this.data.length - 1].date))
+				.attr('y1', yScale(50)).attr('y2', yScale(50));
+			guideGroup.append('line').attr('class', 'guide')
+				.attr('x1', xScale(this.data[0].date)).attr('x2', xScale(this.data[this.data.length - 1].date))
+				.attr('y1', yScale(100)).attr('y2', yScale(100));
+
+			let line = d3.line().curve(d3.curveCatmullRom.alpha(1))
+				.x((d) => xScale(d.date)).y((d) => yScale(d.value));
+			svg.append('path').data([this.data]).attr('d', line).attr('stroke', '#69db7c').attr('fill', 'none');
+		},
+		createSampleData() {
+			var data = [];
+			var now = moment();
+			for( var m = now.clone().subtract(1, 'd'); m.isBefore(now); m.add(1, 'h') ) {
+				data.push({
+					date: m.clone()._d,
+					value: Math.round(Math.random() * 100)
+				});
+			}
+			return data;
 		}
 	},
 	mounted: function() {
@@ -38,5 +69,28 @@ module.exports = {
 .line-graph {
 	width: 100%;
 	height: 150px;
+
+	svg {
+		width: 100%;
+		height: 150px;
+
+		path.domain {
+			display: none;
+		}
+
+		g.y-axis {
+			text {
+				text-anchor: start;
+			}
+		}
+
+		g.tick text {
+			fill: white;
+		}
+
+		line.guide {
+			stroke: #3e3e3e;
+		}
+	}
 }
 </style>
