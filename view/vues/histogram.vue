5787d2683586e87469e8aa0f85e05d283bb6af0a
ckcks12 <wyoducc@gmail.com>
2017-06-02 14:30:46 +0900

    *add

--- view/vues/histogram.vue
+++ view/vues/histogram.vue
@@ -1,16 +1,62 @@
 <template lang='pug'>
-    svg.histogram
+portlet(simple='true')
+    span(slot='title')
+        button.btn.btn-default(@click='changeType(0)') cpu
+        span &nbsp;
+        button.btn.btn-default(@click='changeType(1)') memory
+        span &nbsp;
+        button.btn.btn-default(@click='changeType(2)') ui rendering
 </template>
 
 <script>
-    module.exports = {
-        mounted: function() {
-            console.log('hi');
+export default {
+    props: [],
+    data() {
+        return {
+            app: this.$root.app,
+            data: {},
+            type: 'cpu'
         }
+    },
+    watch: {
+        'app.isInitDone'() {
+            if( ! this.app.isInitDone ) {
+                return
+            }
+            this.fetch()
+        }
+    },
+    methods: {
+        fetch() {
+            this.$http.get(`/api/histogram/${this.app.packageName}/`)
+        },
+        clear() {
+            $(this.$el).find('*').remove()
+        },
+        draw() {
+
+        },
+        changeType(p) {
+            switch( p ) {
+                case 0:
+                    this.type = 'cpu'
+                    break
+                case 1:
+                    this.type = 'memory'
+                    break
+                case 2:
+                    this.type = 'ui'
+                    break
+            }
+        }
+    },
+    mounted() {
+
     }
+}
 </script>
 
-<style lang='sass' scoped>
+<style lang='scss' scoped>
 
 histogram {
 
